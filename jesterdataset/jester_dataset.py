f0ba1caf516bc9543f1c9ce0e5655476d61995eb
FabianH <fabian.hertwig@gmail.com>
2020-02-11 16:43:22 +0100

    Add functionality to load a given number of frames
    The torch dataloader expects all tensores to have the same dimensions. Therefore you can set the number of frames to load from each video. There are different strategies to pad the frames if the video is too short or to select frames from the video if it is too long.

--- jesterdataset/jester_dataset.py
+++ jesterdataset/jester_dataset.py
@@ -1,25 +1,69 @@
+from enum import Enum
+
+from torch import randint
 from torch.utils.data import Dataset
 from PIL import Image
 from pathlib import Path
 
 
 class JesterDataset(Dataset):
+    class FrameSelectStrategy(Enum):
+        FROM_BEGINNING = 0
+        FROM_END = 1
+        RANDOM = 2
 
-    def __init__(self, csv_file, root_dir, video_dir="20bn-jester-v1", file_ending="jpg", video_transform=None):
-        self.file_ending = file_ending
-        self.root_dir = root_dir
-        self.video_dir_name = video_dir
+    class FramePadding(Enum):
+        REPEAT_END = 0
+        REPEAT_BEGINNING = 2
+
+    def __init__(self, csv_file="./jester_data/jester-v1-train.csv", video_dir="./jester_data/20bn-jester-v1", frame_file_ending="jpg", number_of_frames=8,
+                 frame_select_strategy=FrameSelectStrategy.RANDOM, frame_padding=FramePadding.REPEAT_END,
+                 video_transform=None):
+        """
+        A pytorch dataset to load the 20BN-JESTER dataset or datasets in the same format.
+
+        Args:
+            csv_file: Path to the csv file, describing the videos. In jester this is eg. "1;Swipe Right" where 1 is
+                the video folder name
+            video_dir: Path to the directory containing the videos
+            frame_file_ending: File ending of the video images, eg. "jpg" for file names like "0001.jpg"
+            number_of_frames: The number of frames to get from one video.
+            frame_select_strategy: When the video has more frames than number_of_frames, then the frame_select_strategy
+                is used to select a subset of the frames.
+            frame_padding: If the video does not have enough frames, the frame padding strategy is used to fill the frames.
+            video_transform:  Videotransforms to apply to the frames. You can not use the usual torchvision.transforms
+                as the same transform must be applied to all the frames of one video.
+                Checkout https://github.com/hassony2/torch_videovision for video compatible transforms.
+        """
+        self.file_ending = frame_file_ending
+        self.video_dir = video_dir
+        self.number_of_frames = number_of_frames
+        self.frame_select_strategy = frame_select_strategy
+        self.frame_padding = frame_padding
         self.video_transform = video_transform
         self.data_description = self._read_csv(csv_file)
 
     def _read_csv(self, path):
+        result = []
         with open(path, "r") as f:
-            return [line.strip().split(";") for line in f]
+            for line in f:
+                line_contents = line.strip().split(";")
+                if len(line_contents) == 1:
+                    result.append([line_contents[0], None])
+                else:
+                    result.append(line_contents)
+            return result
 
     def __getitem__(self, index):
         video_id, label = self.data_description[index]
-        frame_files = Path(self.root_dir / self.video_dir_name / str(video_id)).glob(f"*.{self.file_ending}")
-        frames = [Image.open(frame_file) for frame_file in frame_files]
+        video_directory = Path(self.video_dir) / str(video_id)
+        frame_files = list(Path(video_directory).glob(f"*.{self.file_ending}"))
+        if len(frame_files) == 0:
+            raise FileNotFoundError(f"Could not find any frames. There should be at least one frame in the directory "
+                                    f"{video_directory}")
+        frame_files = self._add_padding(frame_files, self.number_of_frames, self.frame_padding)
+        frame_files = self._select_frames(frame_files, self.frame_select_strategy, self.number_of_frames)
+        frames = [Image.open(frame_file).convert('RGB') for frame_file in frame_files]
 
         if self.video_transform:
             frames = self.video_transform(frames)
@@ -28,3 +72,40 @@ class JesterDataset(Dataset):
 
     def __len__(self):
         return len(self.data_description)
+
+    def _add_padding(self, frame_files, number_of_frames, frame_padding: FramePadding):
+        difference = number_of_frames - len(frame_files)
+        if difference > 0:
+            if frame_padding == self.FramePadding.REPEAT_BEGINNING:
+                frame_index_to_repeat = 0
+            elif frame_padding == self.FramePadding.REPEAT_END:
+                frame_index_to_repeat = -1
+            else:
+                raise ValueError("Frame Padding Type not supported")
+
+            frame_files += [frame_files[frame_index_to_repeat] for _ in range(difference)]
+
+        return frame_files
+
+    def _select_frames(self, frame_files: list, frame_select_strategy: FrameSelectStrategy, number_of_frames: int):
+        if len(frame_files) <= number_of_frames:
+            return frame_files
+        else:
+            if frame_select_strategy == self.FrameSelectStrategy.FROM_BEGINNING:
+                return frame_files[:number_of_frames]
+            elif frame_select_strategy == self.FrameSelectStrategy.FROM_END:
+                return frame_files[-number_of_frames:]
+            elif frame_select_strategy == self.FrameSelectStrategy.RANDOM:
+                difference = len(frame_files) - number_of_frames
+                random_start_index = randint(0, difference, (1,)).item()
+                end_index = random_start_index + number_of_frames
+                return frame_files[random_start_index:end_index]
+            else:
+                raise ValueError("FrameSelectStrategy not supported.")
+
+
+
+
+
+
+
--- test/test_jester_dataset.py
+++ test/test_jester_dataset.py
@@ -1,39 +1,96 @@
 from unittest import TestCase
-
 from torch.utils.data.dataloader import DataLoader
-from torchvision.transforms import transforms
+from torch_videovision.videotransforms.volume_transforms import ClipToTensor
 
 from test.utils import TestDataSet, project_dir
 from jesterdataset import JesterDataset
 
-from torch_videovision.videotransforms.volume_transforms import ClipToTensor
-
 
 class TestJesterDataset(TestCase):
 
-    def setUp(self) -> None:
-        self.data_dir = project_dir() / "test_data"
-        self.min_number_of_frames = 10
-        self.max_number_of_frames = 20
-        self.train_test_validation_size = (20, 10, 10)
-        self.testDataset = TestDataSet(self.data_dir, self.train_test_validation_size, self.min_number_of_frames,
-                                       self.max_number_of_frames, True)
-        self.testDataset.create()
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.data_dir = project_dir() / "test_data"
+        cls.min_number_of_frames = 10
+        cls.max_number_of_frames = 20
+        cls.train_test_validation_size = (20, 10, 10)
+        cls.testDataset = TestDataSet(cls.data_dir, cls.train_test_validation_size, cls.min_number_of_frames,
+                                      cls.max_number_of_frames, True)
+        cls.testDataset.create()
 
-    def tearDown(self) -> None:
-        self.testDataset.remove()
+        cls.train_csv_file = cls.data_dir / "jester-v1-train.csv"
+        cls.validation_csv_file = cls.data_dir / "jester-v1-validation.csv"
+        cls.video_dir = cls.data_dir / "20bn-jester-v1"
 
-    def test_JesterDataset(self):
-        dataset = JesterDataset(self.data_dir / "jester-v1-train.csv", self.data_dir, video_transform=None)
+    @classmethod
+    def tearDownClass(cls) -> None:
+        # cls.testDataset.remove()
+        pass
+
+    def test_JesterDataset_without_padding(self):
+        number_of_frames = 10
+        self.assertLessEqual(number_of_frames, self.min_number_of_frames)
+        dataset = JesterDataset(self.train_csv_file, self.video_dir, number_of_frames=number_of_frames,
+                                video_transform=None)
         self.assertEqual(len(dataset), self.train_test_validation_size[0])
         for frames, label in dataset:
-            self.assertGreaterEqual(len(frames), self.min_number_of_frames)
-            self.assertLessEqual(len(frames), self.max_number_of_frames)
+            self.assertEqual(len(frames), number_of_frames)
+            self.assertIn(label, self.testDataset.labels)
+
+    def test_JesterDataset_with_validation_set(self):
+        number_of_frames = 10
+        self.assertLessEqual(number_of_frames, self.min_number_of_frames)
+        dataset = JesterDataset(self.validation_csv_file, self.video_dir, number_of_frames=number_of_frames,
+                                video_transform=None)
+        self.assertEqual(len(dataset), self.train_test_validation_size[2])
+        for frames, label in dataset:
+            self.assertEqual(len(frames), number_of_frames)
+            self.assertIsNone(label)
+
+    def test_JesterDataset_with_padding(self):
+        number_of_frames = 15
+        self.assertGreater(number_of_frames, self.min_number_of_frames)
+        dataset = JesterDataset(self.train_csv_file, self.video_dir, number_of_frames=number_of_frames,
+                                video_transform=None)
+        self.assertEqual(len(dataset), self.train_test_validation_size[0])
+        for frames, label in dataset:
+            self.assertEqual(len(frames), number_of_frames)
+            self.assertIn(label, self.testDataset.labels)
+
+    def test_JesterDataset_with_random_frame_select(self):
+        number_of_frames = 5
+        self.assertLess(number_of_frames, self.min_number_of_frames)
+        dataset = JesterDataset(self.train_csv_file, self.video_dir, number_of_frames=number_of_frames,
+                                frame_select_strategy=JesterDataset.FrameSelectStrategy.RANDOM, video_transform=None)
+        self.assertEqual(len(dataset), self.train_test_validation_size[0])
+        for frames, label in dataset:
+            self.assertEqual(len(frames), number_of_frames)
+            self.assertIn(label, self.testDataset.labels)
+
+    def test_JesterDataset_with_frame_select_from_beginning(self):
+        number_of_frames = 5
+        self.assertLess(number_of_frames, self.min_number_of_frames)
+        dataset = JesterDataset(self.train_csv_file, self.video_dir, number_of_frames=number_of_frames,
+                                frame_select_strategy=JesterDataset.FrameSelectStrategy.FROM_BEGINNING,
+                                video_transform=None)
+        self.assertEqual(len(dataset), self.train_test_validation_size[0])
+        for frames, label in dataset:
+            self.assertEqual(len(frames), number_of_frames)
+            self.assertIn(label, self.testDataset.labels)
+
+    def test_JesterDataset_with_frame_select_from_end(self):
+        number_of_frames = 5
+        self.assertLess(number_of_frames, self.min_number_of_frames)
+        dataset = JesterDataset(self.train_csv_file, self.video_dir, number_of_frames=number_of_frames,
+                                frame_select_strategy=JesterDataset.FrameSelectStrategy.FROM_END, video_transform=None)
+        self.assertEqual(len(dataset), self.train_test_validation_size[0])
+        for frames, label in dataset:
+            self.assertEqual(len(frames), number_of_frames)
             self.assertIn(label, self.testDataset.labels)
 
     def test_JesterDataset_with_dataloader(self):
 
-        dataset = JesterDataset(self.data_dir / "jester-v1-train.csv", self.data_dir, video_transform=ClipToTensor())
+        dataset = JesterDataset(self.train_csv_file, self.video_dir, video_transform=ClipToTensor())
 
         dataloader = DataLoader(dataset, batch_size=4,
                                 shuffle=True, num_workers=4)
